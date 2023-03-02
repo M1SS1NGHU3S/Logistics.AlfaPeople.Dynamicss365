c419e0c2d15601d049000498493684fd976647c5
sourbiskit <ruanvictor.qa@gmail.com>
2023-03-02 14:44:11 -0300

    Add project files.

---
+++ Logistics.AlfaPeople.Plugins/Plugins/ProductManager.cs
@@ -0,0 +1,17 @@
+﻿using Logistics.AlfaPeople.Plugins.LogisticsISV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logistics.AlfaPeople.Plugins.Plugins
+{
+	public class ProductManager : PluginCore
+	{
+		public override void ExecutePlugin(IServiceProvider serviceProvider)
+		{
+			this.TracingService.Trace("Entrou no Plugin");
+		}
+	}
+}
