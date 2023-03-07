23b3844ae2c1615a0f3f7f903f67c9b964e56c37
Maria Julia de Oliveira <52303035+majulia@users.noreply.github.com>
2023-03-07 15:48:55 -0300

    feature-task10-captalizacao-do-nome

---
+++ Logistic.AlfaPeople.Customizations/Logistic2023.AlfaPeople.Account.js
@@ -0,0 +1,18 @@
+﻿if (typeof (Logistic) == "undefined") { Logistic = {} }
+if (typeof (Logistic.Account) == "undefined") { Logistic.Account = {} }
+
+
+Logistic.Account = {
+
+    OnChange: function (executionContext) {
+       
+        var formContext = executionContext.getFormContext();
+        var nameAccount = formContext.getAttribute("name").getValue();
+
+        nameAccount = nameAccount.toLowerCase().replace(/(?:^|\s)\S/g, function (w) {
+            return w.toUpperCase();
+        });
+
+        formContext.getAttribute("name").setValue(nameAccount);
+    }
+}
